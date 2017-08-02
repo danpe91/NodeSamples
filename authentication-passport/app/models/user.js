958052691cd2921667e3c0251cfea73ce95f1ff5
Daniel Pedroza <danpe91@gmail.com>
2017-08-01 20:34:55 -0500

    Fix typo on genSaltFunction

--- authentication-passport/app/models/user.js
+++ authentication-passport/app/models/user.js
@@ -28,7 +28,7 @@ var userSchema = mongoose.Schema({
 });
 
 userSchema.methods.generateHash = function(password) {
-	return bcrypt.hashSync(password, bcrypt.getSaltSync(8), null);
+	return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
 userSchema.methods.validPassword = function(password) {
